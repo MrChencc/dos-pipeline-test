74d1bd57c8b95260e75643f4f9bac0d3e8a17f84
congcong.chen <congcong.chen@timeondata.com>
2018-01-19 16:03:58 +0800

    修改 mavenCanaryRelease.groovy

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -12,26 +12,27 @@ def call(body) {
 
 //    sh "git checkout -b ${env.JOB_NAME}-${config.version}"
 //    sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -U -DnewVersion=${config.version}" // 更新统一的版本
-    sh "mvn clean -f pom-project-generator.xml -e -U deploy" // 推送jar包到nexus
+//    sh "mvn clean -f ${config.bulidPom} -e -U deploy" // 推送jar包到nexus
 
     def s2iMode = flow.isOpenShiftS2I()
     echo "s2i mode: ${s2iMode}"
 
-        def m = readMavenPom file: "${config.pom}"
-        def groupId
-        if (m.groupId == null){
-            groupId = m.parent.groupId.split( '\\.' )
-        } else {
-            groupId = m.groupId.split( '\\.' )
-        }
-        def user = groupId[groupId.size()-1].trim()
-        def artifactId = m.artifactId.toLowerCase()
-        def version = m.version
+    def m = readMavenPom file: "${config.pom}"
+    def groupId
+    if (m.groupId == null){
+        groupId = m.parent.groupId.split( '\\.' )
+    } else {
+        groupId = m.groupId.split( '\\.' )
+    }
+    def user = groupId[groupId.size()-1].trim()
+    def artifactId = m.artifactId.toLowerCase()
+    def version = m.version
 
-        def fabric8Registry = env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST+':'+env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT+'/'
+    def fabric8Registry = env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST+':'+env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT+'/'
 
     if (!s2iMode) {
         retry(1){
+            sh "docker build -f ${config.dockerfilePath} -t ${fabric8Registry}${user}/${artifactId}:${config.version} ."
             sh "docker tag ${user}/${artifactId}:${version} ${fabric8Registry}${user}/${artifactId}:${config.version}"
             sh "docker push  ${fabric8Registry}${user}/${artifactId}:${config.version}"
             sh "docker rmi -f ${user}/${artifactId}:${version}"
