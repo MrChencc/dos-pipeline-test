b4e144487165e4bda9f49d7216e13139da644cab
congcong.chen <congcong.chen@timeondata.com>
2018-01-22 10:35:52 +0800

    修改

--- tests/testCostomPipelineUtil.groovy
+++ tests/testCostomPipelineUtil.groovy
@@ -5,6 +5,6 @@ import com.tod.CostomPipelineUtil
 /**
  * --- 测试Json工具类 ---
  */
-def conf = new File('file/costomPipelineConifg.json').getText('UTF-8');
+def conf = new File('file/costomPipelineConfig.json').getText('UTF-8');
 def json = CostomPipelineUtil.getJsonPipelineConfig(conf)
 print(json)
