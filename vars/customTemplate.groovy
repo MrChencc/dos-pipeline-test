e30e3b4877b49f5b804e79b8115cf349f75cc637
congcong.chen <congcong.chen@timeondata.com>
2018-02-08 17:46:49 +0800

    添加customNode

---
+++ vars/customTemplate.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/groovy
+
+def call(Map parameters = [:], body) {
+
+    def defaultLabel = "clients.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_')
+    def label = parameters.get('label', defaultLabel)
+
+    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:0.1')
+    def customImage = parameters.get('customImage', 'dos/jenkins-slave-base')
+    def inheritFrom = parameters.get('inheritFrom', 'base')
+
+    def flow = new io.fabric8.Fabric8Commands()
+
+    echo 'Mounting docker socket to build docker images'
+    podTemplate(label: label, inheritFrom: "${inheritFrom}",
+            containers: [
+                    [name: 'custom', image: "${customImage}", command: 'cat', ttyEnabled: true],
+                    [name: 'clients', image: "${clientsImage}", command: 'cat', ttyEnabled: true, privileged: true]],
+            volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
+                      persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepo'),
+                      secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
+                      secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
+                      secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
+                      secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
+                      secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
+                      hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
+            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
+    ) {
+
+        body(
+
+        )
+    }
+
+}
