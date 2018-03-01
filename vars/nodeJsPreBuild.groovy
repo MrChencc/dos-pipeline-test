55e5b9769e6c5de6af4fd4ff182c2181ac223060
Nightmare <xueyang.zhu@timeondata.com>
2018-03-01 11:33:03 +0800

    尝试使用bash

--- vars/nodeJsPreBuild.groovy
+++ vars/nodeJsPreBuild.groovy
@@ -9,11 +9,13 @@ def call(body) {
     echo 'switch node version...'
     try {
         sh """
+#!/bin/bash        
 source /.nvm/nvm.sh
 nvm use ${customConfig.build.nodeversion}
         """
     } catch (error) {
         sh """
+#!/bin/bash        
 source /.nvm/nvm.sh
 nvm install  ${customConfig.build.nodeversion}
 nvm use ${customConfig.build.nodeversion}
